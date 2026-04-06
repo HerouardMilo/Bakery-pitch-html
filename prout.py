bb86e4f32a5b8626bf9be8ff3cb8417a1bcb9bb9
Milo <96125235+HerouardMilo@users.noreply.github.com>
2026-04-06 16:24:05 +0200

    Update prout.py

--- prout.py
+++ prout.py
@@ -10,13 +10,35 @@ from streaming.simulator import run_simulator
 
 load_dotenv()
 
-logger = logging.getLogger(__name__)
 log_path = os.getenv("log_path")
 
 logging.basicConfig(
-    filename=f"{log_path}/streaming/thread_manager.py.log",
     level=logging.DEBUG,
-    format="%(asctime)s - %(levelname)s - %(message)s"
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.FileHandler(f"{log_path}/streaming.log"),
+        logging.StreamHandler()
+    ]
 )
 
+logger = logging.getLogger(__name__)
 env = os.getenv("ENV")
+
+def run_flush_loop():
+    '''
+    Calls flush function every 5 sec
+    '''
+    while True :
+        flush(env)
+        time.sleep(5)
+        
+if __name__=="__main__":
+    
+    thread_simulator = threading.Thread(target=run_simulator, daemon=True)
+    thread_flush = threading.Thread(target=run_flush_loop, daemon = True)
+    
+    thread_simulator.start()
+    thread_flush.start()
+    
+    thread_simulator.join()
+    thread_flush.join()
